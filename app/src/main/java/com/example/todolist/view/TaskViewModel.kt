e7940c00d62f37d3c9aa338f6faa83b839c82698
ibtihala817 <Ibtihal.ibrahim.dmm.k1@tuwaiq.edu.sa>
2021-10-28 11:53:39 +0300

    edit

--- app/src/main/java/com/example/todolist/view/TaskViewModel.kt
+++ app/src/main/java/com/example/todolist/view/TaskViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.todolist.view
 
-class TaskViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todolist.repository.TaskRepository
+import com.example.todolist.taskdatabase.taskmodel.TaskModel
+import kotlinx.coroutines.launch
+import java.util.*
+
+class TaskViewModel : ViewModel() {
+    private val taskRepository = TaskRepository.get()
+    var taskItems = taskRepository.getItems()
+   // var selectedItemMutableLiveDate = MutableLiveData<TaskModel>()
+   fun addItem(task: String,
+               descripition: String,
+               date: Date,
+               duedate: Date,
+               checkbox: Boolean){
+       viewModelScope.launch {
+           taskRepository.addItem(TaskModel(task,descripition,date,duedate,checkbox))
+       }
+   fun updateItem(taskModel: TaskModel){
+       viewModelScope.launch {
+           taskRepository.updateItem(taskModel)
+       }
+   }
+   fun deleteItem(taskModel: TaskModel){
+       viewModelScope.launch {
+           taskRepository.deleteItem(taskModel)
+       }
+   }
+   }
 }
