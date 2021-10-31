c992650633d94458d17949ce150bca6e79adc97e
ibtihala817 <Ibtihal.ibrahim.dmm.k1@tuwaiq.edu.sa>
2021-10-31 20:29:17 +0300

    edit

--- app/src/main/java/com/example/todolist/view/TaskAdapter.kt
+++ app/src/main/java/com/example/todolist/view/TaskAdapter.kt
@@ -12,6 +12,8 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.R
 import com.example.todolist.taskdatabase.taskmodel.TaskModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TaskAdapter(val items:List<TaskModel>, val viewModel: TaskViewModel):
 RecyclerView.Adapter<TaskAdapter.TaskViewHolder>(){
@@ -51,6 +53,14 @@ RecyclerView.Adapter<TaskAdapter.TaskViewHolder>(){
             viewModel.selectedItemMutableLiveDate.postValue(item)
             it.findNavController().navigate(R.id.action_listTaskFragment_to_taskDetialsFragment)
         }
+
+        var currentDate = Date()
+        val format = SimpleDateFormat("yyyy/MM/dd")
+        val deadline = format.parse(item.duedaue)
+        if (currentDate > deadline){
+            holder.taskTextView.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        }
+
         holder.checkBox.setOnClickListener {
             item.checkbox = holder.checkBox.isChecked
             viewModel.updateItem(item)
